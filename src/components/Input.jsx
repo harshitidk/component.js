f0d78ae2b15339219f244f3487bcb0c7dceb85e8
harshit kumar <harshit.page@gmail.com>
2025-06-25 23:59:26 +0530

    -a

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,34 @@
 import React,{useState} from "react";
 
 function Input(){
-    const [todo, setTodo] = useState(["i will sleep","working on react","learn storytelling"])
+    const [count,setCount] = useState(4);
+    const [todo, setTodo] = useState([
+    { id: 1, text: "i will sleep" },
+    { id: 2, text: "working on react" },
+    { id: 3, text: "learn storytelling" }
+])
 
     function handleAddTodo(event){
         const newtodo = document.getElementById("new-todo").value;
+        if (!newtodo) return;
         document.getElementById("new-todo").value = "";
-        setTodo([...todo,newtodo]);
+        const fixtodo = {id:count,text:newtodo}
+        setTodo(todo=>[...todo,fixtodo]);
+        setCount(prev => prev + 1);
     }
 
-    // function handleRemoveTodo(){
-    // }
+    function handleRemoveTodo(index){
+        setTodo(todo.filter((_,i)=> i != index));
+    }
 
     return(<div>
         <h2>my todo list</h2>
-        <ul>
-            {todo.map((todos, index) => <li key={index}>{todos}</li>)}
-        </ul>
+        <div className="todo">
+            {todo.map((todos, index) => <div className="todo-list" key={todos.id}>
+            <input className="checkbox" type="checkbox" onChange={() => handleRemoveTodo(index)} />
+            <label>{todos.text}</label>
+            </div>)}
+        </div>
         <div className="add-button">
             <input id="new-todo" type="text" />
             <button onClick={handleAddTodo}>Add</button>
