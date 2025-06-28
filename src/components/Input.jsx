761b8aec1608d1c4ef72a6f5e896eb8a184a80df
harshit kumar <harshit.page@gmail.com>
2025-06-28 23:52:31 +0530

    -a

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,7 +6,7 @@ function Input(){
     { id: 1, text: "i will sleep" },
     { id: 2, text: "working on react" },
     { id: 3, text: "learn storytelling" }
-])
+]);
 
     function handleAddTodo(event){
         const newtodo = document.getElementById("new-todo").value;
@@ -21,13 +21,30 @@ function Input(){
         setTodo(todo.filter((_,i)=> i != index));
     }
 
+    function handleup(index){
+        if (index>0){
+            const updated = [...todo];
+            [updated[index],updated[index-1]]=[updated[index-1],updated[index]]
+            setTodo(updated);
+        }
+    }
+
+    function handledown(index){
+        if (index<todo.length-1){
+            const updated = [...todo];
+            [updated[index],updated[index+1]]=[updated[index+1],updated[index]]
+            setTodo(updated);
+        }
+    }
+
+
     return(<div>
         <h2>my todo list</h2>
         <div className="todo">
         {todo.map((todos, index) => <div className="todo-list" key={todos.id}>
             <div className="svg-dad">
-            <div className="svg"><svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 20 20"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg></div>
-            <div className="svg"><svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 20 20"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg></div>
+                <div className="up svg" onClick={()=>handleup(index)}><svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 20 20"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg></div>
+                <div className="down svg" onClick={()=>handledown(index)}><svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 20 20"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg></div>
             </div>
             <input className="checkbox" type="checkbox" 
             onChange={() => handleRemoveTodo(index)} />
